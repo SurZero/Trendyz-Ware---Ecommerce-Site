036f1f2ab9e664d7a53da30ad049d1792242c437
Susil Thapa <51055620+SurZero@users.noreply.github.com>
2021-01-06 19:52:54 +0545

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -160,8 +160,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'thapasusil53@gmail.com'
-# EMAIL_HOST_PASSWORD = 'simw yzhp uelu mjrb'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
